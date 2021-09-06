a8fb7d354cba8f17637e8a1f7b71205d654e2ccb
Jose <jose_esteban_basualdo@hotmail.com>
2021-09-06 17:46:33 -0300

    agrega desafío clase 2

---
+++ desafio_clase1/clase2_basualdo.js
@@ -0,0 +1,35 @@
+const numeroUsuario = parseInt( prompt('Por favor ingresa un número'));
+
+if (numeroUsuario > 1000){
+
+    alert('Tu número es mayor que 1000')
+}
+
+const textoUsuario = prompt('Por favor ingresa una palabra por ej: "Hola" ');
+
+
+if (textoUsuario === "Hola"){
+
+    alert('Tu texto es igual a Hola');
+}
+
+const segundoNumeroUsuario = parseInt( prompt('Por favor ingresa un número'));
+
+if(segundoNumeroUsuario >= 10 && segundoNumeroUsuario <=50 ){
+
+    alert('Tu número está entre 10 y 50');
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
