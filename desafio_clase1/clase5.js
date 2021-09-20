4000e92c819e2501699aa9dfd1576134b38c3926
Jose <jose_esteban_basualdo@hotmail.com>
2021-09-20 17:04:51 -0300

    agrega clase5.js

---
+++ desafio_clase1/clase5.js
@@ -0,0 +1,77 @@
+
+//Calculadora de tiempo promedio en relación a la cantidad de turnos asignados.
+
+function calcularTiempoDeEsperaPromedio(turnosAsignados){
+
+    
+    let promedioEsperaPorTurno = 15;
+
+    let resultadoTiempoDeEspera = turnosAsignados * promedioEsperaPorTurno;
+
+    return resultadoTiempoDeEspera;
+}
+
+function asignarTurno(){
+
+    let mensajeUsuario = prompt('Perfecto! Tu turno ya está registrado!');
+
+    return mensajeUsuario;
+
+}
+function obtenerTurno(){
+
+    let nuevoTurno = prompt('Hola para obtener un nuevo turno ingresa Si, para cancelar ingresa No');
+
+    return nuevoTurno;
+
+}
+
+function revisarRespuestaUsuario(respuestaUsuario){
+
+    if (respuestaUsuario.toLowerCase() === "si" ){
+
+        asignarTurno();
+
+        cantidadDeTurnosAsignados=  cantidadDeTurnosAsignados + 1;
+
+        alert( "Su tiempo de espera es de " + calcularTiempoDeEsperaPromedio(cantidadDeTurnosAsignados) + "minutos");
+
+
+
+        
+
+
+    } else if (respuestaUsuario.toLowerCase() === ""){
+
+        console.log('Lo siento no entendí tu respuesta.');
+        
+    }
+
+    else if (respuestaUsuario.toLowerCase() === "no"){
+
+        console.log('Esperamos tenga buenos días. Saludos !')
+    }
+
+    else {
+        console.log('Lo siento no es una respuesta valida')
+        
+    }
+}
+
+
+let nuevoTurnoUsuario;
+let cantidadDeTurnosAsignados = 0;
+let tiempoDeEspera;
+
+ do { 
+nuevoTurnoUsuario = obtenerTurno();
+
+revisarRespuestaUsuario(nuevoTurnoUsuario);
+
+ }
+
+ while(nuevoTurnoUsuario.toLowerCase() != "no")
+
+
+
+
