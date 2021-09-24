7604b3cd002263c35d9e605010b6084f954b9dce
Jose <jose_esteban_basualdo@hotmail.com>
2021-09-23 22:21:02 -0300

    agrega funciones
    además agrega comentarios con lo que hace cada una.

--- desafio_clase1/proyecto_primera_entrega/root/js/index.js
+++ desafio_clase1/proyecto_primera_entrega/root/js/index.js
@@ -1,5 +1,83 @@
+//Esta función avisa cuando el stock llega a cero.
+
+function controlarStock(productos,opcionUsuario){
+
+    let idElegido = productos.find(producto => producto.id == opcionUsuario);
+    let mensaje;
+
+    if (idElegido.stock <= 0){
+
+        mensaje = alert("Producto sin Stock");
+    }
+
+        return mensaje;
+}
+
+
+
+//Esta función pregunta la cantidad de producto que desea el usuario
+//devuelve dicha cantidad
+function preguntarCantidad(){
+
+    let cantidadUsuario = prompt('Por favor indique la cantidad de unidades');
+
+    return cantidadUsuario;
+}
+
+/*Esta función quita la cantidad de stock comprado por el usuario
+devuelve la cantidad restante de stock segund el id del producto */
+function quitarCantidadAlStock(cantidadUsuario,productos,opcionUsuario){
+
+    
+    let idElegido = productos.find(producto => producto.id == opcionUsuario);
+
+    return idElegido.stock -= cantidadUsuario;
+
+}
+
+/*Esta función calcula el precio  a pagar en base a la cantidad elegida 
+por el usuario devuelve el precio a pagar */
+function calcularPrecio(cantidadUsuario,productos,opcionUsuario){
+
+    let precioPagar;
+   for(let i =0; i< productos.length;i++){
+
+        if(opcionUsuario == productos[i].id){
+
+            precioPagar = cantidadUsuario* productos[i].precio
+
+        }
+   }
+
+   return precioPagar;
+}
+
+/*Esta funcion revisa la respuesta del usuario y activa las demas funciones
+para poder realizar la compra. */
+function revisarRespuestaUsuario(opcionUsuario,productos){
+
+    let cantidadUsuario;
+
+    let coincideId = productos.find(producto => producto.id == opcionUsuario);
+
+    if(coincideId){
+
+        console.log(cantidadUsuario = preguntarCantidad());
+        controlarStock(productos,opcionUsuario);
+        console.log(quitarCantidadAlStock(cantidadUsuario,productos,opcionUsuario));
+        console.log(calcularPrecio(cantidadUsuario,productos,opcionUsuario));
+       // PreguntarSiQuiereSeguirComprando esta opcion aun no la exploré
+
+    }
+    else if(opcionUsuario == "salir"){
+        console.log ('Gracias por su compra')
+    }
+    else console.log('La opción ingresada es incorrecta');
+}
+
 //Esta función muestra los productos disponibles en el menú
 //para poder ser comprados mediante su id,
+
 function mostrarMenu (productos,menu){
 
     productos.forEach(producto => {
@@ -28,18 +106,26 @@ function mostrarMenu (productos,menu){
 
     ];
 
-
+    
 
     const menu  = "Estos son los productos disponibles ";
+    let opcion;
     
+    alert('Hola! Esta es la Tienda de Miel')
     
     do{
 
-       let OPCION ;
-        OPCION = mostrarMenu(productos,menu);
+       
+
+        opcion = mostrarMenu(productos,menu);
+        
+
+        revisarRespuestaUsuario(opcion,productos);
+
+        
 
 
 
     }
    
-    while(OPCION != "salir")
+    while(opcion != "salir")
