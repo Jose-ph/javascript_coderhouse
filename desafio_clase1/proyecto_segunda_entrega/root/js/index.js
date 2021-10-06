ebf7479676b847126d7aee3dafcfffb2e627691a
Jose <jose_esteban_basualdo@hotmail.com>
2021-10-06 18:14:06 -0300

    agrega scripts a tienda.html

--- desafio_clase1/proyecto_segunda_entrega/root/js/index.js
+++ desafio_clase1/proyecto_segunda_entrega/root/js/index.js
@@ -5,40 +5,7 @@
 
 
 
-productos = [
-  {
-    id: 1,
-    nombre: "Miel Pura",
-    peso: "500g",
-    envase: "plástico",
-    precio: 250,
-    stock: 50,
-  },
-  {
-    id: 2,
-    nombre: "Miel Pura",
-    peso: "500g",
-    envase: "vidrio",
-    precio: 350,
-    stock: 60,
-  },
-  {
-    id: 3,
-    nombre: "Miel Pura",
-    peso: "1000g",
-    envase: "plástico",
-    precio: 500,
-    stock: 90,
-  },
-  {
-    id: 4,
-    nombre: "Miel Pura",
-    peso: "1000g",
-    envase: "vidrio",
-    precio: 750,
-    stock: 80,
-  },
-];
+
 
 /*  const menu  = "Estos son los productos disponibles "; */
 let menu = document.getElementById("menu-productos");
