6d3572f638ebfc0c3f3b1ea89c63f945a873f6c0
Jose <jose_esteban_basualdo@hotmail.com>
2021-10-07 21:46:09 -0300

    actualiza funciones.js

--- desafio_clase1/proyecto_segunda_entrega/root/js/funciones.js
+++ desafio_clase1/proyecto_segunda_entrega/root/js/funciones.js
@@ -1,163 +1,200 @@
-//Esta función avisa cuando el stock llega a cero.
+
+ // función para crear cards segun los productos 
+// y la sección deseada
+function crearCards(productos,seccion) {
+  //productos es un array de objetos.
+
+  //let sectionPrueba = document.querySelector('#prueba');
+
+  for (let i = 0; i < productos.length; i++) {
+    
+
+    let cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "card");
+    cardDiv.style.width = "18rem";
+
+    let imgDiv = document.createElement("img");
+    imgDiv.setAttribute("src", "../images/logo3.png");
+    imgDiv.setAttribute("class", "card-img-top");
+
+    let cardBodyDiv = document.createElement("div");
+    cardBodyDiv.setAttribute("class", "card-body");
+    //agrego id prueba
+    cardBodyDiv.setAttribute("id",`${productos[i].id}`);
+
+    let cardTitulo = document.createElement("h5");
+    cardTitulo.setAttribute("class", "card-title");
+    cardTitulo.innerText = `${productos[i].id}-- ${productos[i].nombre}`;
+
+    let cardParrafo = document.createElement("p");
+    cardParrafo.setAttribute("class", "card-text");
+    cardParrafo.textContent = ` Precio: $ ${productos[i].precio}-- stock: ${productos[i].stock}`;
+
+    let cardEnlace = document.createElement("a");
+    cardEnlace.setAttribute("class", "btn btn-primary");
+    //cardEnlace.setAttribute("href", "#");
+    cardEnlace.innerText = "Agregar al carrito";
+
+    // agregar todos los elementos al body div
+
+    cardBodyDiv.appendChild(cardTitulo);
+    cardBodyDiv.appendChild(cardParrafo);
+    cardBodyDiv.appendChild(cardEnlace);
+
+    //agregar elementos al card div principal
+
+    cardDiv.appendChild(imgDiv);
+    cardDiv.appendChild(cardBodyDiv);
+
+    //sectionPrueba.appendChild(cardDiv);
+    seccion.appendChild(cardDiv);
+  }
+}
+
+//esta funcion revisa la card seleccionada por el usuario
+
+function revisarOpcionUsuario (opcion,productos){
+
+  let coincideId = productos.find((producto) => producto.id == opcion);
+  
+
+  if(coincideId){
+
+  let indice = productos.findIndex(producto => producto.id == opcion);
+    
+    arrayDeSeleccionados.push(productos[indice]);
+
+  }
+
+  return arrayDeSeleccionados;
+}
+
+
+//esta función cuenta las repeticiones en un array
+//para poder descontar del stock
+
+function contarRepeticiones (array){
+
+  let arrayId = [];
+  let repeticiones ={};
+
+  array.forEach(element => {
+
+    arrayId.push( element.id);
+    
+  });
+  
+  for (const num of arrayId) {
+    repeticiones[num] = repeticiones[num] ? repeticiones[num] + 1 : 1;
+  }
+  
+  
+  console.log(repeticiones[1])
+  console.log(repeticiones[2])
+
+  console.log(repeticiones[3])
+  console.log(repeticiones[4])
+
+
+}
+
+// esta funcion agrega cards al carrito
+function agregarAlCarritoDom (productosSeleccionados){
+
+    crearCards(productosSeleccionados,sectionCarrito);
+    
+    
+
+}
+
+ //Esta función avisa cuando el stock llega a cero.
 
 function controlarStock(productos, opcionUsuario) {
-    let idElegido = productos.find((producto) => producto.id == opcionUsuario);
-    let mensaje;
-  
-    if (idElegido.stock <= 0) {
-      mensaje = alert("Producto sin Stock");
-    }
-  
-    return mensaje;
+  let idElegido = productos.find((producto) => producto.id == opcionUsuario);
+  let mensaje;
+
+  if (idElegido.stock <= 0) {
+    mensaje = alert("Producto sin Stock");
   }
 
-  
-  //Esta función pregunta la cantidad de producto que desea el usuario
+  return mensaje;
+}
+
+//Esta función pregunta la cantidad de producto que desea el usuario
 //devuelve dicha cantidad
 function preguntarCantidad() {
-    let cantidadUsuario = prompt("Por favor indique la cantidad de unidades");
-  
-    return cantidadUsuario;
-  }
+  let cantidadUsuario = prompt("Por favor indique la cantidad de unidades");
 
-  
+  return cantidadUsuario;
+}
 
 /*Esta función quita la cantidad de stock comprado por el usuario
 devuelve la cantidad restante de stock segund el id del producto */
 function quitarCantidadAlStock(cantidadUsuario, productos, opcionUsuario) {
-    let idElegido = productos.find((producto) => producto.id == opcionUsuario);
-  
-    return (idElegido.stock -= cantidadUsuario);
-  }
+  let idElegido = productos.find((producto) => producto.id == opcionUsuario);
 
-  
-  /*Esta función calcula el precio  a pagar en base a la cantidad elegida 
+  return (idElegido.stock -= cantidadUsuario);
+}
+
+/*Esta función calcula el precio  a pagar en base a la cantidad elegida 
 por el usuario devuelve el precio a pagar */
 function calcularPrecio(cantidadUsuario, productos, opcionUsuario) {
-    let precioPagar;
-    for (let i = 0; i < productos.length; i++) {
-      if (opcionUsuario == productos[i].id) {
-        precioPagar = cantidadUsuario * productos[i].precio;
-      }
+  let precioPagar;
+  for (let i = 0; i < productos.length; i++) {
+    if (opcionUsuario == productos[i].id) {
+      precioPagar = cantidadUsuario * productos[i].precio;
     }
-  
-    return precioPagar;
   }
 
+  return precioPagar;
+}
 
-  /*Esta funcion revisa la respuesta del usuario y activa las demas funciones
+/*Esta funcion revisa la respuesta del usuario y activa las demas funciones
 para poder realizar la compra. */
 function revisarRespuestaUsuario(opcionUsuario, productos) {
-    let cantidadUsuario;
-  
-    let coincideId = productos.find((producto) => producto.id == opcionUsuario);
-  
-    if (coincideId) {
-      cantidadUsuario = preguntarCantidad();
-      controlarStock(productos, opcionUsuario);
-      quitarCantidadAlStock(cantidadUsuario, productos, opcionUsuario);
-      calcularPrecio(cantidadUsuario, productos, opcionUsuario);
-    } else if (opcionUsuario == "salir") {
-      const mensajeFinCompra = document.createElement("h2");
-  
-      mensajeFinCompra.innerText = "Gracias por su compra";
-  
-      document.body.appendChild(mensajeFinCompra);
-    } else {
-      let mensajeOpcionIncorrecta = document.createElement("h2");
-  
-      mensajeOpcionIncorrecta.innerText = "La opción ingresada es incorrecta";
-      document.body.appendChild(mensajeOpcionIncorrecta);
-    }
-  }
+  let cantidadUsuario;
 
-  //Esta función muestra los productos disponibles en el menú
+  let coincideId = productos.find((producto) => producto.id == opcionUsuario);
+
+  if (coincideId) {
+    cantidadUsuario = preguntarCantidad();
+    controlarStock(productos, opcionUsuario);
+    quitarCantidadAlStock(cantidadUsuario, productos, opcionUsuario);
+    calcularPrecio(cantidadUsuario, productos, opcionUsuario);
+  } else if (opcionUsuario == "salir") {
+    const mensajeFinCompra = document.createElement("h2");
+
+    mensajeFinCompra.innerText = "Gracias por su compra";
+
+    document.body.appendChild(mensajeFinCompra);
+  } else {
+    let mensajeOpcionIncorrecta = document.createElement("h2");
+
+    //mensajeOpcionIncorrecta.innerText = "La opción ingresada es incorrecta";
+    //document.body.appendChild(mensajeOpcionIncorrecta);
+  }
+}
+
+//Esta función muestra los productos disponibles en el menú
 //para poder ser comprados mediante su id,
 
 function mostrarMenu(productos, menu) {
-    menu.innerHTML = "";
-    productos.forEach((producto) => {
-      /*  menu += `
-                  ${producto.id} ${producto.nombre} ${producto.envase} ${producto.peso}
-                  
-                   ` */
-  
-      menu.innerHTML += ` <ul> 
-                 <li>  ${producto.id} ${producto.nombre} ${producto.envase} ${producto.peso} </li>
-  
-                  </ul>
-                  
-                  `;
-    });
-  
-    menu += "Elige un número para comprar ese producto, o salir para terminar";
-    let opcionUsuario = prompt(menu);
-    return opcionUsuario;
-  }
+  menu.innerHTML = "";
+  productos.forEach((producto) => {
+    /*  menu += `
+                ${producto.id} ${producto.nombre} ${producto.envase} ${producto.peso}
+                
+                 ` */
 
-  // función para crear cards segun los productos
+    menu.innerHTML += ` <ul> 
+               <li>  ${producto.id} ${producto.nombre} ${producto.envase} ${producto.peso} </li>
 
-  function crearCards(productos) {
-    //productos es un array de objetos.
-  
-    //let sectionPrueba = document.querySelector('#prueba');
-  
-    for (let i = 0; i < productos.length; i++) {
-      console.log(Object.keys(productos[i]));
-      console.log(Object.values(productos[i]));
-  
-      let cardDiv = document.createElement("div");
-      cardDiv.setAttribute("class", "card");
-      cardDiv.style.width = "18rem";
-  
-      let imgDiv = document.createElement("img");
-      imgDiv.setAttribute("src", "../images/logo3.png");
-      imgDiv.setAttribute("class", "card-img-top");
-  
-      let cardBodyDiv = document.createElement("div");
-      cardBodyDiv.setAttribute("class", "card-body");
-  
-      let cardTitulo = document.createElement("h5");
-      cardTitulo.setAttribute("class", "card-title");
-      cardTitulo.innerText = `${productos[i].id}-- ${productos[i].nombre}`;
-  
-      let cardParrafo = document.createElement("p");
-      cardParrafo.setAttribute("class", "card-text");
-      cardParrafo.textContent = ` Precio: $ ${productos[i].precio}-- stock: ${productos[i].stock}`;
-  
-      let cardEnlace = document.createElement("a");
-      cardEnlace.setAttribute("class", "btn btn-primary");
-      cardEnlace.setAttribute("href", "#");
-      cardEnlace.setAttribute("id","carrito");
-      cardEnlace.innerText = "Agregar al carrito";
-  
-      // agregar todos los elementos al body div
-  
-      cardBodyDiv.appendChild(cardTitulo);
-      cardBodyDiv.appendChild(cardParrafo);
-      cardBodyDiv.appendChild(cardEnlace);
-  
-      //agregar elementos al card div principal
-  
-      cardDiv.appendChild(imgDiv);
-      cardDiv.appendChild(cardBodyDiv);
-  
-      sectionPrueba.appendChild(cardDiv);
-    }
-  }
-  
+                </ul>
+                
+                `;
+  });
 
-  //esta funcion guarda los productos en un array 
-
-  function cargarCarrito (producto){
-
-    let carrito = [];
-
-    carrito.push(producto);
-
-    return carrito;
-
-
-
-
-  }
+  menu += "Elige un número para comprar ese producto, o salir para terminar";
+  let opcionUsuario = prompt(menu);
+  return opcionUsuario;
+}
