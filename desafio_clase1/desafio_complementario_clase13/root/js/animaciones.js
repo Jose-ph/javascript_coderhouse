e13708e452d28860e9feb57e845167f1f89f71be
Jose <jose_esteban_basualdo@hotmail.com>
2021-10-12 22:00:27 -0300

    update animaciones.js

--- desafio_clase1/desafio_complementario_clase13/root/js/animaciones.js
+++ desafio_clase1/desafio_complementario_clase13/root/js/animaciones.js
@@ -2,11 +2,19 @@
 
 let botonMostrar = $('#mostrar');
 
+//Animaciones de las cards
 botonMostrar.click(()=>{
 
     $('#fila-servicios').fadeIn(1000,function(){
 
-        $('#fila-servicios').fadeOut(800);
+        $('#fila-servicios').fadeOut(1100);
+        $('#img-logo').toggle("fast");
+
+        $('#img-carrito').css("width", "5%");
+
+        $('#img-carrito').fadeIn().fadeOut();
+       
+
 
     });
 
