d423235512b9bef9a6f79dbe3e8778c2f19cde62
Jose <jose_esteban_basualdo@hotmail.com>
2021-10-15 09:58:02 -0300

    agrega post con ajax y jquery

--- desafio_clase1/desafio_entregable_clase14/root/js/index.js
+++ desafio_clase1/desafio_entregable_clase14/root/js/index.js
@@ -98,11 +98,37 @@ let datosUsuario = [];
 
    botonEnviar.click(() => {
 
+    const URLGET = "https://jsonplaceholder.typicode.com/posts"
+
       datosUsuario.push( nombreUsuario.val());
        datosUsuario.push( apellidoUsuario.val()) ;
       datosUsuario.push( dniUsuario.val());
        datosUsuario.push( paisUsuario.val());
 
       console.log(datosUsuario);
+
+      const infoEnviar = JSON.stringify(datosUsuario);
+
+      $.post(URLGET,infoEnviar,(respuesta,estado)=>{
+
+        if(estado === "success"){
+
+          $('#bienvenida').hide();
+          $('#form-js').hide();
+          $('#fila-servicios').hide();
+          $('#menu-productos').hide();
+          $('#carrito').prepend(`<h2>Envio de información exitoso</h2>`)
+        }
+
+        else console.log("ERROR DE ENVÍO");
+
+
+
+      });
+
+
+
+
+     
    });
   
